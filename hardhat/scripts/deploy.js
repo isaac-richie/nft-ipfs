2c54d8da6319a216e6bfcfa9e1921a9f6aab5dae
adeyemi isaac <isaacoulwaseun66@gmail.com>
2022-10-03 18:21:58 +0100

    init

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,61 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+/*const hre = require("hardhat");
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+  const lockedAmount = hre.ethers.utils.parseEther("1");
+
+  const Lock = await hre.ethers.getContractFactory("Lock");
+  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+  await lock.deployed();
+
+  console.log(
+    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+}); */
+
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+
+async function main() {
+  // URL from where we can extract the metadata for a LW3Punks
+  const metadataURL = "ipfs://YOUR-METADATA-CID/";
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so lw3PunksContract here is a factory for instances of our LW3Punks contract.
+  */
+  const lw3PunksContract = await ethers.getContractFactory("LW3Punks");
+
+  // deploy the contract
+  const deployedLW3PunksContract = await lw3PunksContract.deploy(metadataURL);
+
+  await deployedLW3PunksContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("LW3Punks Contract Address:", deployedLW3PunksContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
---
+++ my-app/constants/index.js
@@ -0,0 +1,66 @@
+export const NFT_CONTRACT_ADDRESS = "0x503FCF41F16D88B7eb9A715a843064F3dC552696";
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_unlockTime",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "payable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "when",
+          "type": "uint256"
+        }
+      ],
+      "name": "Withdrawal",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "owner",
+      "outputs": [
+        {
+          "internalType": "address payable",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "unlockTime",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "withdraw",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    }
+  ];
